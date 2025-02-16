344f8221a1aa41859678bb391546d139ca4471df
Hiperagil_ <pinteaanddrei12@gmail.com>
2025-02-16 15:54:53 +0200

    Carousel update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,10 @@ import Carousel3D from "./CustomCarousel"; // Importă CustomCarousel
 
 const hairstyles = [
   { name: "Tuns Simplu", price: "45 lei", duration: "30 min", details: "Acest servciu este pentru barbati cu parul scurt si copii. La aceasta tunsoare nu se face skin fade.", imagen: "/images/haircut.jpg" },
-  { name: "Tund Modern", price: "55 lei", duration: "40 min", details: "Skin fade, Low/Mid/High tapper, drop.", imagen: "/images/shave.jpg" },
+  { name: "Tuns Modern", price: "55 lei", duration: "40 min", details: "Skin fade, Low/Mid/High tapper, drop.", imagen: "/images/shave.jpg" },
   { name: "Pachet Premium", price: "100 lei", duration: "60 lei", details: "Servicii VIP, Tunds, Spalat, Masaj Capilar, Aranjarea parului. ", imagen: "/images/beardtrim.jpg" },
   { name: "Tuns barbati si contur barba", price: "65 lei", duration: "50 min", details: "Tuns pentru barbati si aranjarea barbii la forma si lungimea dorita.", imagen: "/images/hairdye.jpg" },
-  { name: "Tund Modern", price: "55 lei", duration: "40 min", details: "Skin fade, Low/Mid/High tapper, drop.", imagen: "/images/shave.jpg" },
+  { name: "Tuns Modern", price: "55 lei", duration: "40 min", details: "Skin fade, Low/Mid/High tapper, drop.", imagen: "/images/shave.jpg" },
 ];
 
 function App() {
